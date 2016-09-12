6ba485c985f939052abac50db79cb401459c3de0
SI内山 <r.uchiyama@sint.co.jp>
2016-09-12 18:56:16 +0900

    init

---
+++ Crypter/tools/Crypto.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tools
+{
+    partial class Crypto
+    {
+        private System.Security.Cryptography.SymmetricAlgorithm algorithm;
+
+        public int BlockSize
+        {
+            set
+            {
+                if (algorithm != null)
+                {
+
+                    algorithm.BlockSize = value;
+                }
+            }
+            get
+            {
+                if (algorithm != null)
+                {
+                    return algorithm.BlockSize;
+                }
+                return -1;
+            }
+        }
+        public int KeySize;
+        public System.Security.Cryptography.CipherMode mode;
+
+    }
+
+    /// <summary>
+    /// いらないんじゃない？？？
+    /// 
+    /// </summary>
+    class CryptoType
+    {
+        private System.Security.Cryptography.SymmetricAlgorithm algorithm;
+
+        public int BlockSize
+        {
+            set {
+                if (algorithm != null )
+                {
+                    algorithm.BlockSize = value;
+                }
+            }
+            gey { }
+        }
+        public int KeySize;
+        public System.Security.Cryptography.CipherMode mode;
+
+
+
+    }
+}
