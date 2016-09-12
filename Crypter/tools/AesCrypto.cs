4419fab58de29862b5c4b42dbba73890c5e401db
Ryota Uchiyama <lessironglance@gmail.com>
2016-09-12 23:37:21 +0900

    Create Abstract Class Create Aes Class

---
+++ Crypter/tools/AesCrypto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tools
+{
+    class AesCrypto : Crypto
+    {
+        public override SymmetricAlgorithm Algorithm
+        {
+            get
+            {
+                if(algorithm == null || algorithm.GetType() != typeof(AesCryptoServiceProvider))
+                {
+                    algorithm = new AesCryptoServiceProvider();
+                }
+
+                return algorithm;
+            }
+        }
+    }
+}
--- Crypter/tools/Crypto.cs
+++ Crypter/tools/Crypto.cs
@@ -1,14 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace tools
 {
-    partial class Crypto
+    abstract class Crypto
     {
-        private System.Security.Cryptography.SymmetricAlgorithm algorithm;
+        protected SymmetricAlgorithm algorithm;
+
+        public abstract SymmetricAlgorithm Algorithm { get; }
 
         public int BlockSize
         {
@@ -16,46 +19,194 @@ namespace tools
             {
                 if (algorithm != null)
                 {
-
-                    algorithm.BlockSize = value;
+                    bool success = false;
+                    foreach (KeySizes k in algorithm.LegalBlockSizes)
+                    {
+                        if (k.MinSize <= value && value <= k.MaxSize)
+                        {
+                            algorithm.BlockSize = value;
+                            success = true;
+                            break;
+                        }
+                    }
+                    if (!success)
+                    {
+                        throw new CryptographicException("ブロック サイズが無効です。");
+                    }
+                }
+                else
+                {
+                    throw new NullReferenceException("Algorithm is Null.");
                 }
             }
+
             get
             {
                 if (algorithm != null)
                 {
                     return algorithm.BlockSize;
                 }
-                return -1;
-            }
-        }
-        public int KeySize;
-        public System.Security.Cryptography.CipherMode mode;
-
-    }
-
-    /// <summary>
-    /// いらないんじゃない？？？
-    /// 
-    /// </summary>
-    class CryptoType
-    {
-        private System.Security.Cryptography.SymmetricAlgorithm algorithm;
-
-        public int BlockSize
-        {
-            set {
-                if (algorithm != null )
+                else
                 {
-                    algorithm.BlockSize = value;
+                    throw new NullReferenceException("Algorithm is Null.");
                 }
             }
-            gey { }
         }
-        public int KeySize;
-        public System.Security.Cryptography.CipherMode mode;
 
+        public int KeySize
+        {
+            set
+            {
+                if (algorithm != null)
+                {
+                    bool success = false;
+                    foreach (KeySizes k in algorithm.LegalKeySizes)
+                    {
+                        if (k.MinSize <= value && value <= k.MaxSize)
+                        {
+                            algorithm.KeySize = value;
+                            success = true;
+                            break;
+                        }
+                    }
+                    if (!success)
+                    {
+                        throw new CryptographicException("キー サイズが有効ではありません。");
+                    }
+                }
+                else
+                {
+                    throw new NullReferenceException("Algorithm is Null.");
+                }
+            }
 
+            get
+            {
+                if (algorithm != null)
+                {
+                    return algorithm.BlockSize;
+                }
+                else
+                {
+                    throw new NullReferenceException("Algorithm is Null.");
+                }
+            }
+        }
 
+        public int FeedbackSize
+        {
+            set
+            {
+                if (algorithm != null)
+                {
+                    if (0 <= value && value <= algorithm.BlockSize)
+                    {
+                        algorithm.FeedbackSize = value;
+                    }
+                    else
+                    {
+                        throw new CryptographicException("フィードバック サイズがブロック サイズを超えています。");
+                    }
+                }
+                else
+                {
+                    throw new NullReferenceException("Algorithm is Null.");
+                }
+            }
+
+            get
+            {
+                if (algorithm != null)
+                {
+                    return algorithm.FeedbackSize;
+                }
+                else
+                {
+                    throw new NullReferenceException("Algorithm is Null.");
+                }
+            }
+        }
+
+        public CipherMode Mode
+        {
+            set
+            {
+                if (algorithm != null)
+                {
+                    algorithm.Mode = value;
+                }
+                else
+                {
+                    throw new NullReferenceException("Algorithm is Null.");
+                }
+            }
+
+            get
+            {
+                if (algorithm != null)
+                {
+                    return algorithm.Mode;
+                }
+                else
+                {
+                    throw new NullReferenceException("Algorithm is Null.");
+                }
+            }
+        }
+
+        public PaddingMode Padding
+        {
+            set
+            {
+                if (algorithm != null)
+                {
+                    algorithm.Padding = value;
+                }
+                else
+                {
+                    throw new NullReferenceException("Algorithm is Null.");
+                }
+            }
+
+            get
+            {
+                if (algorithm != null)
+                {
+                    return algorithm.Padding;
+                }
+                else
+                {
+                    throw new NullReferenceException("Algorithm is Null.");
+                }
+            }
+        }
+
+        public byte[] Key
+        {
+            //TODO:Exception
+            set
+            {
+                algorithm.Key = value;
+            }
+
+            get
+            {
+                return algorithm.Key;
+            }
+        }
+
+        public byte[] IV
+        {
+            //TODO:Exception
+            set
+            {
+                algorithm.IV = value;
+            }
+
+            get
+            {
+                return algorithm.IV;
+            }
+        }
     }
 }
